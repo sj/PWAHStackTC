0b090a89444a0ebc65a111e2758bb8c8afbbf4fd
bas <bas@840f488b-753d-4fb4-b916-1b28b8a18e6e>
2010-04-10 22:13:17 +0000

    Create empty class for WAH compressed BitSet
    git-svn-id: https://guust.tuxes.nl/svn/bas/studie/0910-afstuderen/Implementations/c++@400 840f488b-753d-4fb4-b916-1b28b8a18e6e

---
+++ WAHStackTC/src/WAHBitSet.cpp
@@ -0,0 +1,17 @@
+/*
+ * WAHBitSet.cpp
+ *
+ *  Created on: Apr 10, 2010
+ *      Author: bas
+ */
+
+#include "WAHBitSet.h"
+
+WAHBitSet::WAHBitSet() {
+	// TODO Auto-generated constructor stub
+
+}
+
+WAHBitSet::~WAHBitSet() {
+	// TODO Auto-generated destructor stub
+}
---
+++ WAHStackTC/src/WAHBitSet.h
@@ -0,0 +1,17 @@
+/*
+ * WAHBitSet.h
+ *
+ *  Created on: Apr 10, 2010
+ *      Author: bas
+ */
+
+#ifndef WAHBITSET_H_
+#define WAHBITSET_H_
+
+class WAHBitSet {
+public:
+	WAHBitSet();
+	virtual ~WAHBitSet();
+};
+
+#endif /* WAHBITSET_H_ */
