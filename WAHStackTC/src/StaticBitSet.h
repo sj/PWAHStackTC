ba46c444fb116034e290e06ffbc7e532be73d9e1
bas <bas@840f488b-753d-4fb4-b916-1b28b8a18e6e>
2010-05-18 11:15:59 +0000

    Re-introduce static bit set with smart clear functionality
    git-svn-id: https://guust.tuxes.nl/svn/bas/studie/0910-afstuderen/Implementations/c++@443 840f488b-753d-4fb4-b916-1b28b8a18e6e

---
+++ WAHStackTC/src/StaticBitSet.h
@@ -0,0 +1,36 @@
+/*
+ * StaticBitSet.h
+ *
+ *  Created on: May 17, 2010
+ *      Author: bas
+ */
+
+#ifndef STATICBITSET_H_
+#define STATICBITSET_H_
+#include <string>
+#include "BitSet.h"
+
+class StaticBitSet : public BitSet {
+private:
+	unsigned int _capacity;
+	int _firstBitSet, _lastBitSet;
+	unsigned int _numLongs;
+	long* _bits;
+
+public:
+	StaticBitSet(int capacity);
+	virtual ~StaticBitSet();
+
+	void init(int initialCapacity = 64);
+
+	void clear();
+	void set(int bitIndex);
+	void set(int bitIndex, bool value);
+	bool get(int bitIndex);
+	string toString();
+	string toBitString(long value);
+	unsigned int capacity();
+	unsigned int size();
+};
+
+#endif /* STATICBITSET_H_ */
