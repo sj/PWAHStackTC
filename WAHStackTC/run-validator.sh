649be7b89551121d26474c7e5d9742ad3209884d
bas <bas@840f488b-753d-4fb4-b916-1b28b8a18e6e>
2010-05-20 11:02:02 +0000

    validator now creates clean checkout and starts building and running from there
    git-svn-id: https://guust.tuxes.nl/svn/bas/studie/0910-afstuderen/Implementations/c++@460 840f488b-753d-4fb4-b916-1b28b8a18e6e

--- WAHStackTC/run-validator.sh
+++ WAHStackTC/run-validator.sh
@@ -1,39 +1,55 @@
 #!/bin/bash
 
+svnusername="wahstacktc-validator"
+svnurl="https://guust.tuxes.nl/svn/bas/studie/0910-afstuderen/Implementations/c++/WAHStackTC"
 logfile="/home/bas/wahstacktc-validator/`date +%s`.log"
 #useconfiguration="Debug"
 useconfiguration="Release"
-
-if [ ! -d "$useconfiguration" ]; then
-	echo "Can not find build configuration dir '$useconfiguration' in current directory?!" >&2
-	exit 1
-fi
 
 if [ ! -d "`dirname $logfile`" ]; then
 	mkdir "`dirname $logfile`"
 fi
 
+tmpdir=`mktemp -d`
+cd $tmpdir
+
 date >> $logfile
-svn up &> /dev/null
-svn info src | grep Revision >> $logfile
+echo -n "Checking out latest revision in $tmpdir... " >> $logfile
+svn --username=$svnusername co "$svnurl" . > /dev/null
+svnres="$?"
+
+if [ ! "$svnres" = "0" ]; then
+	echo "FAILED!" >> $logfile
+	echo "Checkout failed!" >&2
+
+	rm -rf "$tmpdir"
+	exit 1
+fi
+
+svn info | grep Revision >> $logfile
+
+if [ ! -d "$useconfiguration" ]; then
+	echo "Can not find build configuration dir '$useconfiguration' in checkout directory?!" >> $logfile
+	echo "Can not find build configuration dir '$useconfiguration' in checkout directory?!" >&2
+	rm -rf "$tmpdir"
+	exit 1
+fi
+
 echo >> $logfile
 echo >> $logfile
 echo "======= Build log =======" >> $logfile
 
-tmplogfile=`mktemp`
 cd $useconfiguration
-make all &> $tmplogfile
+(make all 2>&1) >> $logfile
 buildres="$?"
-cat $tmplogfile >> $logfile
 echo >> $logfile
 echo >> $logfile
 
 if [ "$buildres" = "0" ]; then
-	./WAHStackTC --run-validator &> $tmplogfile
+	(./WAHStackTC --run-validator 2>&1) >> $logfile
 	validatorres="$?"
-	cat $tmplogfile >> $logfile
 fi
-rm $tmplogfile
+rm -rf "$tmpdir"
 
 if [ ! "$buildres" = "0" ] || [ ! "$validatorres" = "0" ]; then
 	echo "WAHStackTC validator reported one or more errors!" >&2
